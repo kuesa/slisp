d1bce06c812cbd04892a523d984c332e64634e46
cvs2hg <cvs2hg@localhost>
2000-03-07 13:13:34 +0000

    fixup commit for branch 'unlabeled-1.17.2'

--- src/version.h
+++
@@ -1,6 +0,0 @@
-/* Generated automatically from src/VERSION. */
-#define VERSION "2000-03-06"
-#define VERSION_YYYY 2000
-#define VERSION_YYYY_STRING "2000"
-#define VERSION_MM 3
-#define VERSION_DD 6
